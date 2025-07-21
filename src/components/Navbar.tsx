2025ad93868ceba0f67a052dfda732153673dd8a
KuldeepJha5176 <ranacjha@gmail.com>
2025-07-21 23:41:40 +0530

    completed navbar and badge

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Navbar = () => {
+  const links = [
+    {
+      title: "Guide",
+      href: "#",
+    },
+    {
+      title: "Pricing",
+      href: "#",
+    },
+    {
+      title: "Login",
+      href: "#",
+    },
+  ];
+
+  return (
+    <div className="navbar-root">
+      <div className="logo">Fintech</div>
+      <div className="links">
+        {links.map((link, idx) => (
+          <a className="links-item" href={link.href}>
+            {link.title}
+          </a>
+        ))}
+        <button className="btn">Start Free Trail</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
