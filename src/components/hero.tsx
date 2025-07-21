5610994559ab14ca1d4380787a9bb446b27b067b
KuldeepJha5176 <ranacjha@gmail.com>
2025-07-22 00:54:04 +0530

    completed hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
+    <div className='hero-root'>
      <div className='badge'>
         <span>
 
@@ -10,6 +10,10 @@ const Hero = () => {
         </span>
         
         <svg width="16" height="16" fill="none"><path stroke="#1E1F25" stroke-linecap="round" stroke-linejoin="round" stroke-opacity=".5" stroke-width="1.25" d="M8 4.75 11.25 8m0 0L8 11.25M11.25 8h-6.5"></path></svg>
+     </div>
+     <div>
+        <h1 className='hero-title'>Magically simplify accounting and taxes</h1>
+        <p className='hero-subtitle'>Automated bookkeeping, effortless tax filing, real‑time insights. Set up in 10 mins. Back to building by 12:14am.</p>
      </div>
     </div>
   )
