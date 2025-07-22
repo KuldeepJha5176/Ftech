0ebda71031d0a07fe1d3036db4e24a655f3a7753
KuldeepJha5176 <ranacjha@gmail.com>
2025-07-22 23:46:38 +0530

    added image in hero sec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,17 @@ import Hero from "@/components/hero";
 export default function Home() {
   return (
     <div className="layout">
+      <div className="layout-lines-container">
+        <div className="left-line" />
+        <div className="right-line" />
+      </div>
       <Container>
-      
-
-        <Navbar/>
-        <Hero/>
+        <Navbar />
+        <Hero />
       </Container>
+      <div className="hero-image-container">
+        <img src="/hero-ui-v5.webp" className="hero-img" />
+      </div>
     </div>
   );
 }
